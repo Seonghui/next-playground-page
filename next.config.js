6dbad238e4ccc4d1a997f380af82617d9bb9d85e
유성희 <shy@sk.com>
2023-10-29 11:26:26 +0900

    api 통신 설정

--- next.config.js
+++ next.config.js
@@ -4,6 +4,16 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
+  async rewrites() {
+    return [
+      {
+        // ex. /api/proxy
+        source: `${process.env.NEXT_PUBLIC_API_BASE_PATH}/:match*`,
+        // ex. http://localhost:8000
+        destination: `${process.env.API_BASE_URL}/:match*`,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
